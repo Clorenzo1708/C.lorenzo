dd92564c20a05384c571b9f638eefd48de7bf8ff
Clorenzo1708 <57103342+Clorenzo1708@users.noreply.github.com>
2019-10-28 10:06:12 -0500

    final project

---
+++ finalproject/FinalProjevt.java
@@ -0,0 +1,22 @@
+package finalproject;
+
+/**
+ *
+ *FinalProjevt.java - description
+ * @teacher Mr.Wachs
+ * @author Charles Lorenzo
+ * @since Oct. 11, 2019, 10:10:59 a.m.
+ */
+public class FinalProjevt {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+       new gamee().setVisible(true);
+  
+       
+    }
+
+}
