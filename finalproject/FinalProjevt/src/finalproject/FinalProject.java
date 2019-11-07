cf3e749f42adf8894e7201246ca43ae571359efa
Clorenzo1708 <57103342+Clorenzo1708@users.noreply.github.com>
2019-11-07 10:53:58 -0600

    Add files via upload

---
+++ finalproject/FinalProjevt/src/finalproject/FinalProject.java
@@ -0,0 +1,41 @@
+package finalproject;
+
+/**
+ *
+ *FinalProjevt.java - description
+ * @teacher Mr.Wachs
+ * @author Charles Lorenzo
+ * @since Oct. 11, 2019, 10:10:59 a.m.
+ */
+public class FinalProject {
+;
+     final static String []cards = new String [52];
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+       new gamee().setVisible(true);
+       
+    
+    
+    card1();
+    rules();
+
+    }
+
+    private static void card1() {
+         for (int i = 1; i <= cards.length; ) {
+             
+             
+    }
+
+}
+
+    private static void rules() {
+       
+        
+    }
+}
+    
